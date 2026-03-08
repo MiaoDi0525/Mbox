b80e5d82c3fcff8a48341c8c112c28b393d89c94
MiaoDi0525 <miaodi0525@gmail.com>
2026-03-08 18:39:02 +0800

    Create eryder.java

---
+++ src/eryder.java
@@ -0,0 +1,86 @@
+public class ERyder {
+    private String bikeID;
+    private int batteryLevel;
+    private boolean isAvailable;
+    private double kmDriven;
+
+    public ERyder() {
+        this.bikeID = "DEFAULT_BIKE_001";
+        this.batteryLevel = 100;
+        this.isAvailable = true;
+        this.kmDriven = 0.0;
+    }
+
+    public ERyder(String bikeID, int batteryLevel, boolean isAvailable, double kmDriven) {
+        this.bikeID = bikeID;
+        setBatteryLevel(batteryLevel);
+        this.isAvailable = isAvailable;
+        this.kmDriven = kmDriven;
+    }
+
+    public void ride() {
+        if (isAvailable && batteryLevel > 0) {
+            System.out.println("The bike is available.");
+        } else {
+            System.out.println("The bike is not available.");
+        }
+    }
+
+    public void printBikeDetails() {
+        System.out.println("Bike ID: " + bikeID);
+        System.out.println("Battery Level: " + batteryLevel + "%");
+        System.out.println("Availability: " + (isAvailable ? "Available" : "Not Available"));
+        System.out.println("Distance Travelled: " + kmDriven + " km");
+        System.out.println("------------------------------");
+    }
+
+    public String getBikeID() {
+        return bikeID;
+    }
+
+    public void setBikeID(String bikeID) {
+        this.bikeID = bikeID;
+    }
+
+    public int getBatteryLevel() {
+        return batteryLevel;
+    }
+
+    public void setBatteryLevel(int batteryLevel) {
+        if (batteryLevel >= 0 && batteryLevel <= 100) {
+            this.batteryLevel = batteryLevel;
+        } else {
+            System.out.println("Invalid battery level. Must be between 0 and 100.");
+            this.batteryLevel = 0;
+        }
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    public double getKmDriven() {
+        return kmDriven;
+    }
+
+    public void setKmDriven(double kmDriven) {
+        this.kmDriven = kmDriven;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        ERyder defaultBike = new ERyder();
+        System.out.println("--- Default Bike Details ---");
+        defaultBike.printBikeDetails();
+
+        ERyder parameterBike = new ERyder("BIKE_002", 75, true, 120.5);
+        parameterBike.ride();
+        System.out.println("--- Parameter Bike Details ---");
+        parameterBike.printBikeDetails();
+    }
+}
